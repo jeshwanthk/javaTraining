c34ce9ffb0067a735320688894d1e29ef514ed2d
jeshwanthk <147578213+jeshwanthk@users.noreply.github.com>
2025-02-12 16:28:00 +0530

    Add files via upload

---
+++ IncrementOpr.java
@@ -0,0 +1,20 @@
+
+public class IncrementOpr
+{
+	public static void main(String[] args) {
+	    int x=10;
+	   // int y=x++ + x++ + x++;
+	   // int z=++x + ++x + ++x;
+	    //int x_y=++x + x++ + ++x + x++;
+	   // int v=--x + x++ + ++x + --x + x++;
+	    int u=--x + --x + --x + --x;
+	    
+	    
+// 		System.out.println(y);
+// 		System.out.println(z);
+//		System.out.println(x_y);
+//		System.out.println(v);
+		System.out.println(u);
+		
+	}
+}
