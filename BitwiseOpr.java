257f90b4eae1f3443fb45ec398182bd0fc81a714
jeshwanthk <147578213+jeshwanthk@users.noreply.github.com>
2025-02-12 12:36:11 +0530

    Add files via upload

---
+++ BitwiseOpr.java
@@ -0,0 +1,15 @@
+public class BitwiseOpr
+{
+	public static void main(String[] args) {
+	   int a=492;
+	   int b=333;
+	   //int c=58;
+	   int c= a&b;
+	   int d=a|b;
+	   int e= a^b;
+	      System.out.println(c);
+	  	  System.out.println(d);
+      	  System.out.println(e);
+
+	}
+}
