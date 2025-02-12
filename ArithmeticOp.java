9775c1c13308b6547422660fcbb3270c50d4e9f8
jeshwanthk <147578213+jeshwanthk@users.noreply.github.com>
2025-02-12 11:34:18 +0530

    Add files via upload

---
+++ ArithmeticOp.java
@@ -0,0 +1,18 @@
+public class ArithmeticOp
+{
+	public static void main(String[] args) {
+	    byte a=100;
+	    byte b=98;
+	    byte c= (byte)(a+b);
+	    byte d=(byte)(a*b);
+	    byte e=(byte)(a/b);
+	    byte f=(byte)(a%b); 
+	     byte g=(byte)(a-b);
+	    
+		        System.out.println(c);
+				System.out.println(e );
+				System.out.println(d);
+        		System.out.println(f);
+        	    System.out.println(g);
+	}
+}
