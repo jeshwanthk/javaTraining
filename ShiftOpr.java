541300b8b77ec1491ed3b5a795fec5bb1443441c
jeshwanthk <147578213+jeshwanthk@users.noreply.github.com>
2025-02-12 13:00:19 +0530

    Add files via upload

---
+++ ShiftOpr.java
@@ -0,0 +1,9 @@
+public class ShiftOpr
+{
+	public static void main(String[] args) {
+	   int a=10;
+
+	   System.out.println("right shift:"+(a>>2));
+      System.out.println("left shift:"+(a<<2));
+	}
+}
